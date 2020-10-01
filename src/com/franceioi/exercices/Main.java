b8c189ea9cc8bb8b8d5ba9efd705a7abe3c3807a
vinsbld <vincent.blndel@gmail.com>
2020-10-01 16:27:19 +0200

    LeSecretDuGoma

--- src/com/franceioi/exercices/Main.java
+++ src/com/franceioi/exercices/Main.java
@@ -4,15 +4,11 @@ class Main {
 
     public static void main(String[] args) {
 
-        for (int loop = 1; loop <= 2; loop ++) {
-            gauche();
-        }
-        System.out.println("Bonjour, laissez-moi vous aider" );
-        ramasser();
-
-        for (int i = 1; i<=32; i++){
+        for (int loop = 1; loop <= 15; loop ++) {
             droite();
-        }deposer();
-
+            ramasser();
+        }
+        droite();
+        deposer();
     }
 }
