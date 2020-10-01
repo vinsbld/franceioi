b7e2388afb83c33a66fa7146e42466b09a73038a
vinsbld <vincent.blndel@gmail.com>
2020-10-01 17:21:13 +0200

    Vendanges

--- src/com/franceioi/exercices/Main.java
+++ src/com/franceioi/exercices/Main.java
@@ -4,20 +4,17 @@ class Main {
 
     public static void main(String[] args) {
 
-        for (int i =1;i<=108;i++) {
 
-            for (int loop = 1; loop <= 13; loop++) {
-                haut();
-            }
-            for (int loop = 1; loop <= 13; loop++) {
+        for (int i =1;i<=20;i++) {
+            ramasser();
+            for (int loop = 1; loop <= 15; loop++) {
                 droite();
-            }
-            for (int loop = 1; loop <= 13; loop++) {
-                bas();
-            }
-            for (int loop = 1; loop <= 13; loop++) {
+            }deposer();
+
+            for (int loop2 = 1; loop2 <= 15; loop2++) {
                 gauche();
             }
         }
     }
 }
+
