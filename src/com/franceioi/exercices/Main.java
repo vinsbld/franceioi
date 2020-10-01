31573c7936d1915f13e45ed4010e2870dae253ee
vinsbld <vincent.blndel@gmail.com>
2020-10-01 15:56:03 +0200

    MathematiqueDeBaseCorrect

--- src/com/franceioi/exercices/Main.java
+++ src/com/franceioi/exercices/Main.java
@@ -5,7 +5,6 @@ class Main {
     public static void main(String[] args) {
 
         for (int loop = 1; loop <= 13; loop ++) {
-            System.out.println(loop);
             System.out.println("9 * 8 = 72");
         }
     }
