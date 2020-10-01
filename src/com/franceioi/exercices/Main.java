bf4a7ba231626e061811ab17e473730a90ab0dcb
vinsbld <vincent.blndel@gmail.com>
2020-10-01 17:04:54 +0200

    MontKailash

--- src/com/franceioi/exercices/Main.java
+++ src/com/franceioi/exercices/Main.java
@@ -4,17 +4,20 @@ class Main {
 
     public static void main(String[] args) {
 
-        for (int grandeLooop = 1; grandeLooop<=20; grandeLooop++) {
+        for (int i =1;i<=108;i++) {
 
-            for (int loop = 1; loop <= 20; loop++) {
-                System.out.print("OX");
+            for (int loop = 1; loop <= 13; loop++) {
+                haut();
             }
-            System.out.println();
-            for (int loop = 1; loop <= 20; loop++) {
-                System.out.print("XO");
+            for (int loop = 1; loop <= 13; loop++) {
+                droite();
             }
-            System.out.println();
+            for (int loop = 1; loop <= 13; loop++) {
+                bas();
+            }
+            for (int loop = 1; loop <= 13; loop++) {
+                gauche();
+            }
         }
-
     }
 }
