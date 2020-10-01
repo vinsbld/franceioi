7ceca16281e3da687e51691ef5ee01f43210ed64
vinsbld <vincent.blndel@gmail.com>
2020-10-01 17:39:06 +0200

    LeGrandEvenement

--- src/com/franceioi/exercices/Main.java
+++ src/com/franceioi/exercices/Main.java
@@ -1,19 +1,28 @@
 package com.franceioi.exercices;
 
+
 class Main {
 
     public static void main(String[] args) {
+        for (int i =1;i<=9;i++){
+            haut();
+        }
+        for (int i =1;i<=9;i++){
+            droite();
+        }bas();
 
+        for (int j=1;j<=4;j++) {
 
-        for (int i =1;i<=20;i++) {
-            ramasser();
-            for (int loop = 1; loop <= 15; loop++) {
-                droite();
-            }deposer();
-
-            for (int loop2 = 1; loop2 <= 15; loop2++) {
+            for (int i = 1; i <= 8; i++) {
                 gauche();
             }
+            bas();
+            for (int i = 1; i <= 8; i++) {
+                droite();
+            }bas();
+        }
+        for (int i = 1; i <= 9; i++) {
+            gauche();
         }
     }
 }
