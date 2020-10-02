17ce6d9cdbb8e5a7a42d034e67e66ce3b41a9a21
vinsbld <vincent.blndel@gmail.com>
2020-10-02 14:03:13 +0200

    CourseAvecLesEnfants

--- src/com/franceioi/exercices/Main.java
+++ src/com/franceioi/exercices/Main.java
@@ -4,14 +4,66 @@ package com.franceioi.exercices;
 class Main {
 
     public static void main(String[] args) {
-        int nbBonbons = 1;
-        System.out.println(nbBonbons);
-        nbBonbons = 3;
-        System.out.println(nbBonbons);
-        for (int i = nbBonbons; i<=50; i++){
-            nbBonbons = nbBonbons + i;
-            System.out.println(nbBonbons);
-        }
+        droite();
+        ramasser();
+        gauche();
+        deposer();
+        for (int i =1;i<=2;i++){
+            droite();
+        }ramasser();
+        for (int i =1;i<=2;i++){
+            gauche();
+        }deposer();
+        for (int i =1;i<=3;i++){
+            droite();
+        }ramasser();
+        for (int i =1;i<=3;i++){
+            gauche();
+        }deposer();
+        for (int i =1;i<=4;i++){
+            droite();
+        }ramasser();
+        for (int i =1;i<=4;i++){
+            gauche();
+        }deposer();
+        for (int i =1;i<=5;i++){
+            droite();
+        }ramasser();
+        for (int i =1;i<=5;i++){
+            gauche();
+        }deposer();
+
+        for (int i =1;i<=6;i++){
+            droite();
+        }ramasser();
+        for (int i =1;i<=6;i++){
+            gauche();
+        }deposer();
+        for (int i =1;i<=7;i++){
+            droite();
+        }ramasser();
+        for (int i =1;i<=7;i++){
+            gauche();
+        }deposer();
+        for (int i =1;i<=8;i++){
+            droite();
+        }ramasser();
+        for (int i =1;i<=8;i++){
+            gauche();
+        }deposer();
+        for (int i =1;i<=9;i++){
+            droite();
+        }ramasser();
+        for (int i =1;i<=9;i++){
+            gauche();
+        }deposer();
+        for (int i =1;i<=10;i++){
+            droite();
+        }ramasser();
+        for (int i =1;i<=10;i++){
+            gauche();
+        }deposer();
+
     }
 }
 
