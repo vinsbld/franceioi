ab15c885641cddaa7941c6242c937cf9d334030c
vinsbld <vincent.blndel@gmail.com>
2020-10-02 10:42:48 +0200

    TheAnswerIs42

--- src/com/franceioi/exercices/Main.java
+++ src/com/franceioi/exercices/Main.java
@@ -4,26 +4,7 @@ package com.franceioi.exercices;
 class Main {
 
     public static void main(String[] args) {
-        for (int i =1;i<=9;i++){
-            haut();
-        }
-        for (int i =1;i<=9;i++){
-            droite();
-        }bas();
-
-        for (int j=1;j<=4;j++) {
-
-            for (int i = 1; i <= 8; i++) {
-                gauche();
-            }
-            bas();
-            for (int i = 1; i <= 8; i++) {
-                droite();
-            }bas();
-        }
-        for (int i = 1; i <= 9; i++) {
-            gauche();
-        }
+        System.out.println(42);
     }
 }
 
