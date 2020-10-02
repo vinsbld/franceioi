d1cbeb4c98c2889cf11ece9ba742415bdc9c5fd3
vinsbld <vincent.blndel@gmail.com>
2020-10-02 14:07:40 +0200

    CourseAvecLesEnfantsOptimum

--- src/com/franceioi/exercices/Main.java
+++ src/com/franceioi/exercices/Main.java
@@ -4,66 +4,16 @@ package com.franceioi.exercices;
 class Main {
 
     public static void main(String[] args) {
-        droite();
-        ramasser();
-        gauche();
-        deposer();
-        for (int i =1;i<=2;i++){
-            droite();
-        }ramasser();
-        for (int i =1;i<=2;i++){
-            gauche();
-        }deposer();
-        for (int i =1;i<=3;i++){
-            droite();
-        }ramasser();
-        for (int i =1;i<=3;i++){
-            gauche();
-        }deposer();
-        for (int i =1;i<=4;i++){
-            droite();
-        }ramasser();
-        for (int i =1;i<=4;i++){
-            gauche();
-        }deposer();
-        for (int i =1;i<=5;i++){
-            droite();
-        }ramasser();
-        for (int i =1;i<=5;i++){
-            gauche();
-        }deposer();
-
-        for (int i =1;i<=6;i++){
-            droite();
-        }ramasser();
-        for (int i =1;i<=6;i++){
-            gauche();
-        }deposer();
-        for (int i =1;i<=7;i++){
-            droite();
-        }ramasser();
-        for (int i =1;i<=7;i++){
-            gauche();
-        }deposer();
-        for (int i =1;i<=8;i++){
-            droite();
-        }ramasser();
-        for (int i =1;i<=8;i++){
-            gauche();
-        }deposer();
-        for (int i =1;i<=9;i++){
-            droite();
-        }ramasser();
-        for (int i =1;i<=9;i++){
-            gauche();
-        }deposer();
-        for (int i =1;i<=10;i++){
-            droite();
-        }ramasser();
-        for (int i =1;i<=10;i++){
-            gauche();
-        }deposer();
-
+      int anneau = 1;
+      for (int i =1;i<=10;i++){
+          for (int j =1; j<=anneau;j++){
+              droite();
+          }ramasser();
+          for (int k =1; k<=anneau;k++){
+              gauche();
+          }deposer();
+          anneau = anneau+i;
+      }
     }
 }
 
