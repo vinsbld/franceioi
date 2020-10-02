c0da45e5bb4b80b977b51b971fc4bf5f1a1aabb8
vinsbld <vincent.blndel@gmail.com>
2020-10-02 16:00:23 +0200

    TableDeMultiplication

--- src/com/franceioi/exercices/Main.java
+++ src/com/franceioi/exercices/Main.java
@@ -3,13 +3,13 @@ package com.franceioi.exercices;
 
 class Main {
     public static void main(String[] args) {
-        int nbCubes = 0;
-        int largeurArete = 1;
+    int nbAmultiplier = 1;
+    for (int i = 1; i<=20; i++){
+        System.out.print(i);
+        for (int j = 2;j<=20;j++){
+            System.out.print(" "+i*j);
+        }System.out.println();
+    }
 
-        for (int i = 1; i<=9; i++){
-            nbCubes = nbCubes + largeurArete * largeurArete * largeurArete;
-            largeurArete = largeurArete + 2;
-
-        }System.out.println(nbCubes);
     }
 }
