5b600d86931c91ce29fff2a70156f369fbc118df
vinsbld <vincent.blndel@gmail.com>
2020-10-02 13:27:53 +0200

    DecollageFusee

--- src/com/franceioi/exercices/Main.java
+++ src/com/franceioi/exercices/Main.java
@@ -5,13 +5,10 @@ class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("V");
-        System.out.println("V");
-        System.out.println("I");
-        System.out.println("I");
-        System.out.println("V");
-        System.out.println("I");
-        System.out.println("I");
+        for (int i = 100;i>=0;i--){
+            System.out.println(i);
+        }
+        System.out.println("Décollage ! ");
     }
 }
 
