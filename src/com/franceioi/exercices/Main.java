5f68338593d09d30331d3f4491354245d076c52a
vinsbld <vincent.blndel@gmail.com>
2020-10-02 13:21:51 +0200

    ProgresserParLerreur

--- src/com/franceioi/exercices/Main.java
+++ src/com/franceioi/exercices/Main.java
@@ -4,10 +4,15 @@ package com.franceioi.exercices;
 class Main {
 
     public static void main(String[] args) {
-    for (int i = 1; i<=100; i++){
-        System.out.println(i);
-    }
-        System.out.println("J'arrive !");
+
+        System.out.println("V");
+        System.out.println("V");
+        System.out.println("I");
+        System.out.println("I");
+        System.out.println("V");
+        System.out.println("I");
+        System.out.println("I");
     }
 }
+
 
